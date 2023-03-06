8f10fd5d87915fd04a400cb0b80025b2d21c34c9
Oisin Hearne <oisinhearne@gmail.com>
2023-03-06 09:46:08 +0000

    string interlopation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-binding-app';
+  count: number = 0;
+
+  incrementCount() {
+    this.count++;
+  }
+
+  imageDblClick() {
+    
+  }
 }
